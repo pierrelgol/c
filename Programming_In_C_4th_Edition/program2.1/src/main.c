c6497d9ae9d1ebe265e013ce14953d1696ad1bfa
Pierre <pierrelgol@gmail.com>
2023-05-03 12:12:05 +0100

    exercices2.1

---
+++ Programming_In_C_4th_Edition/program2.1/src/main.c
@@ -0,0 +1,52 @@
+/*
+ *
+ *   Title of project : 
+ *   Date of creation : 
+ *   Author(s) : PierreLgol
+ *   github : https://github.com/pierrelgol
+ * 
+ *   More on the project :
+ *   
+ *   #TODO(s) :  
+ * 
+ *
+ */
+
+//--------------------[     DEPENDENCIES DECLARATION     ]--------------------//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//--------------------[     CONSTANT DEFINITION     ]--------------------//
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+//--------------------[     STRUCT DEFINITION     ]--------------------//
+
+
+
+//--------------------[     FUNCTION DEFINITION     ]--------------------//
+
+
+
+//--------------------[     MAIN METHOD     ]--------------------//
+
+int main()
+{
+
+ printf("Programming is fun\n");
+
+return EXIT_SUCCESS;
+}
+
+//--------------------------------------------------------------------------//
+
+
+
+//--------------------[     FUNCTION IMPLEMENTATION     ]--------------------//
+
+
+
+//--------------------------------------------------------------------------//
