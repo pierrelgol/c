bcfcb8ad295eb3164c5c1dd74e9973861f456b64
Pierre <pierrelgol@gmail.com>
2023-05-03 12:29:07 +0100

    second exercice

---
+++ Programming_In_C_4th_Edition/program2.2/src/main.c
@@ -0,0 +1,43 @@
+/*
+ *
+ *   Title of project :
+ *   Date of creation : 03/05/2023
+ *   Author(s) : PierreLgol
+ *   github : https://github.com/pierrelgol
+ *
+ *   More on the project :
+ *
+ *   #TODO(s) :
+ *
+ *
+ */
+
+//--------------------[     DEPENDENCIES DECLARATION     ]--------------------//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//--------------------[     CONSTANT DEFINITION     ]--------------------//
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+//--------------------[     STRUCT DEFINITION     ]--------------------//
+
+//--------------------[     FUNCTION DEFINITION     ]--------------------//
+
+//--------------------[     MAIN METHOD     ]--------------------//
+
+int main() {
+
+  printf("Programming is fun\n");
+  printf("Programming in C is even more fun\n");
+
+  return EXIT_SUCCESS;
+}
+
+//--------------------------------------------------------------------------//
+
+//--------------------[     FUNCTION IMPLEMENTATION     ]--------------------//
+
+//--------------------------------------------------------------------------//
