2afc7e1c78ff51670d3f103d993844d470f1a0a2
Pierre <pierrelgol@gmail.com>
2023-05-03 12:39:55 +0100

    program2.3

---
+++ Programming_In_C_4th_Edition/program2.3/src/main.c
@@ -0,0 +1,51 @@
+/*
+ *
+ *   Title of project : Displaying multiple lines of Output
+ *   Date of creation : 03/05/2023
+ *   Author(s) : PierreLgol
+ *   github : https://github.com/pierrelgol
+ * 
+ *   More on the project :
+ *   
+ *   #TODO(s) :  
+ * 
+ *
+ */
+
+//--------------------[     DEPENDENCIES DECLARATION     ]--------------------//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//--------------------[     CONSTANT DEFINITION     ]--------------------//
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+//--------------------[     STRUCT DEFINITION     ]--------------------//
+
+
+
+//--------------------[     FUNCTION DEFINITION     ]--------------------//
+
+
+
+//--------------------[     MAIN METHOD     ]--------------------//
+
+int main()
+{
+	printf("Testing...\n..1\n...2\n....3\n");
+
+return EXIT_SUCCESS;
+}
+
+//--------------------------------------------------------------------------//
+
+
+
+//--------------------[     FUNCTION IMPLEMENTATION     ]--------------------//
+
+
+
+//--------------------------------------------------------------------------//
