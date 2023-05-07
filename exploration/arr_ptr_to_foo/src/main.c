82cf8052efdf1f7109c86c6c92685977c7ff2838
Pierre <pglol.perso@gmail.com>
2023-05-07 15:41:00 +0200

    file

--- exploration/arr_ptr_to_foo/src/main.c
+++ exploration/arr_ptr_to_foo/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -16,8 +16,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-
 
 //--------------------[     METHOD DECLARATION     ]-------------------------//
 
@@ -28,40 +26,34 @@ void division(int a, int b);
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
-	int a, b, choice;
-	void (*foo[])(int, int) = {addition,substraction,multiplication,division};
+int main() {
+  int a, b, choice;
+  void (*foo[])(int, int) = {addition, substraction, multiplication, division};
 
-	
-	printf("Choice of opperation : \n0 - addition\n1 - substraction\n2 - multiplication\n3 - division\n");
-	scanf("%d",&choice);
-	
-	printf("Enter A\n");
-	scanf("%d",&a);
-	
-	printf("Enter B\n");
-	scanf("%d",&b);
+  printf("Choice of opperation : \n0 - addition\n1 - substraction\n2 - "
+         "multiplication\n3 - division\n");
+  scanf("%d", &choice);
 
-	foo[choice](a,b);
+  printf("Enter A\n");
+  scanf("%d", &a);
 
-return EXIT_SUCCESS;
+  printf("Enter B\n");
+  scanf("%d", &b);
+  addition(a, b);
+  foo[choice](a, b);
+
+  return EXIT_SUCCESS;
 }
 //--------------------[     METHOD IMPLEMENTATION    ]-----------------------//
 
-void addition(int a, int b){
-	printf("a plus b = %d\n",(a+b));
-}
-void substraction(int a, int b){
-		printf("a minus b = %d\n",(a-b));
-}
-void multiplication(int a, int b){
-		printf("a multiplyed by b = %d\n",(a*b));
+void addition(int a, int b) { printf("a plus b = %d\n", (a + b)); }
+void substraction(int a, int b) { printf("a minus b = %d\n", (a - b)); }
+void multiplication(int a, int b) {
+  printf("a multiplyed by b = %d\n", (a * b));
 }
 
-void division(int a, int b){
-	if ( b != 0){
-		printf("a divided b = %d\n",(a/b));
-	}
+void division(int a, int b) {
+  if (b != 0) {
+    printf("a divided b = %d\n", (a / b));
+  }
 }
-
