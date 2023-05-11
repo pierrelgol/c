bde244c7bb5146fc119f35b417521be050b24564
Pierre <pierrelgol@gmail.com>
2023-05-11 12:11:35 +0100

    Exo2

--- Programming_In_C_4th_Edition/chapter2/exercices_chapt2.2/src/main.c
+++ Programming_In_C_4th_Edition/chapter2/exercices_chapt2.2/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,11 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+int main() {
+  printf("In C, lowercase letters are significant.\n");
+  printf("main() is where program execution begins.\n");
+  printf("Opening and closing braces enclose program statements in a routine.\n");
+  printf("All program statements must be terminated by a semicolon.\n");
 
-
-return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
