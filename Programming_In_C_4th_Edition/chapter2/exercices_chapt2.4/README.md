2b937fe8f0f9d3cbd7adfba87f48e4d81feed1d7
Pierre <pierrelgol@gmail.com>
2023-05-11 12:18:29 +0100

    exo4

--- Programming_In_C_4th_Edition/chapter2/exercices_chapt2.4/README.md
+++ Programming_In_C_4th_Edition/chapter2/exercices_chapt2.4/README.md
@@ -1,1 +1,1 @@
-# new_project_template
+# 4. Write a program that subtracts the value 15 from 87 and displays the result, together with an appropriate message, at the terminal.
--- Programming_In_C_4th_Edition/chapter2/exercices_chapt2.4/src/main.c
+++ Programming_In_C_4th_Edition/chapter2/exercices_chapt2.4/src/main.c
@@ -18,10 +18,19 @@
 #include <stdlib.h>
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
-
+/* Program to substract 15 from 87 */
 int main()
 {
+ //declaring variables
+ int value1, value2, sum;
 
+ //initializing variables and operating substraction
+ value1 = 87;
+ value2 = 15;
+ sum = value1 - value2;
 
+ //displaying result
+ printf("The result of %d, and %d is %d\n",value1,value2,sum);
+ 
 return EXIT_SUCCESS;
 }
