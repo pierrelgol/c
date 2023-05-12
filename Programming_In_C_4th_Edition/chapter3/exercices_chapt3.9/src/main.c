3f54aa849d08c55a36d80c184fcb962271d229de
Pierre <pierrelgol@gmail.com>
2023-05-12 09:47:08 +0100

    exo9

--- Programming_In_C_4th_Edition/chapter3/exercices_chapt3.9/src/main.c
+++ Programming_In_C_4th_Edition/chapter3/exercices_chapt3.9/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,17 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+int main() {
 
+  int i1 = 365, i3 = 996, j1 = 7, j2 = 23, j3 = 4;
+  float i2 = 12.258;
+  int res1, res2, res3;
 
-return EXIT_SUCCESS;
+  res1 = i1 + j1 - i1 % j1;
+  res2 = i2 + j2 - (int)(i2) % j2;
+  res3 = i3 + j3 - i3 % j3;
+
+  printf("The result 1 is %d, the result 2 is %d, the result 3 is %d\n",res1, res2, res3);
+  
+  return EXIT_SUCCESS;
 }
