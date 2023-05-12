cc04087b3fb11634a954817aded8913bdbbffca9
Pierre <pierrelgol@gmail.com>
2023-05-12 13:43:42 +0100

    exo11

--- Programming_In_C_4th_Edition/chapter4/exercices_chapt4.11/README.md
+++ Programming_In_C_4th_Edition/chapter4/exercices_chapt4.11/README.md
@@ -1,1 +1,1 @@
-# new_project_template
+# 11. Write a program that calculates the sum of the digits of an integer. For example, the sum of the digits of the number 2155 is 2 + 1 + 5 + 5 or 13. The program should accept any arbitrary integer typed in by the user.
--- Programming_In_C_4th_Edition/chapter4/exercices_chapt4.11/src/main.c
+++ Programming_In_C_4th_Edition/chapter4/exercices_chapt4.11/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,26 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+/*11. Write a program that calculates the sum of the digits of an integer. For
+      example, the sum of the digits of the number 2155 is 2 + 1 + 5 + 5 or 13.
+      The program should accept any arbitrary integer typed in by the user. */
 
+int main() { 
 
-return EXIT_SUCCESS;
+  int nput, nput_sum = 0;
+
+  printf("Enter a number to sum up digits\n");
+  scanf("%d",&nput);
+  // get abs value of nput.
+  nput *= ( nput > 0 ? 1 : -1 );
+  
+  printf("The sum of the digits of %d is",nput);
+  do{
+    nput_sum += (nput % 10);
+    nput /= 10; 
+  }while( nput != 0 );
+
+  printf(" %d\n",nput_sum);
+  
+  return EXIT_SUCCESS; 
 }
