5910fab1745d889385749cd309bcb085ca0c4ba5
Pierre <pierrelgol@gmail.com>
2023-05-12 12:15:21 +0100

    exo4

--- Programming_In_C_4th_Edition/chapter4/exercices_chapt4.4/src/main.c
+++ Programming_In_C_4th_Edition/chapter4/exercices_chapt4.4/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,19 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+int main() {
+  
+  int n = 1, i, j;
 
+  printf("|    n    |    n!   |\n");
+  printf("|---------|---------|\n");
+  for (i = 1; i <= 10; i++) {
+    for (j = i; j > 0; j--) {
+      n *= j;
+    }
+    printf("| %7d | %7d |\n", i, (n));
+    n = 1;
+  }
 
-return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
