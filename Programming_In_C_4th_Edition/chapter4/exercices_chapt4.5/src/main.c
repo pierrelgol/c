6aff608054be74fb3107612e9e4858f401ef4276
Pierre <pierrelgol@gmail.com>
2023-05-12 12:28:18 +0100

    exo5

--- Programming_In_C_4th_Edition/chapter4/exercices_chapt4.5/src/main.c
+++ Programming_In_C_4th_Edition/chapter4/exercices_chapt4.5/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,22 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+int main(void) {
 
+  int n, n_pow2;
 
-return EXIT_SUCCESS;
+  printf("TABLE OF POWERS OF TWO\n\n");
+  printf("| n |    n^2    |\n");
+  printf("|---|-----------|\n");
+
+  n_pow2 = 1;
+
+  for (n = 1; n <= 10; ++n) {
+    printf("|%2i |    %5i  |\n", n, n_pow2);
+    n_pow2 *= 2;
+  }
+
+  printf("|---|-----------|\n");
+
+  return 0;
 }
