8fdb3cf3f6928c0199c95794c235304b82c63ff2
Pierre <pierrelgol@gmail.com>
2023-05-12 12:36:35 +0100

    exo6

--- Programming_In_C_4th_Edition/chapter4/exercices_chapt4.6/src/main.c
+++ Programming_In_C_4th_Edition/chapter4/exercices_chapt4.6/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,20 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+/* Program to calculate the 200th triangular number
+ Introduction of the for statement */
 
+int main(void) {
+  int n, triangularNumber;
+  triangularNumber = 0;
 
-return EXIT_SUCCESS;
+  for (n = 1; n <= 200; n = n + 1)
+    triangularNumber = triangularNumber + n;
+
+  printf ("%-2i %i\n", n, triangularNumber); 
+  printf ("%2i %i\n", n, triangularNumber); 
+  printf("The %ith triangular number is %i\n", n, triangularNumber);
+  printf("The %-2ith triangular number is %i\n",n ,triangularNumber);
+
+  return 0;
 }
