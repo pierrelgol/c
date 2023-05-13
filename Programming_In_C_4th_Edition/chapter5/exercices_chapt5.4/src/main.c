258d0fa0f7298fab6ab0a9abe572e427a3e01afe
plgol <plgol.perso@gmail.com>
2023-05-13 16:13:59 +0100

    exo4

--- Programming_In_C_4th_Edition/chapter5/exercices_chapt5.4/src/main.c
+++ Programming_In_C_4th_Edition/chapter5/exercices_chapt5.4/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,48 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+int main() {
 
+  int n = 0, accu = 0, res = 0;
+  char op;
 
-return EXIT_SUCCESS;
+  do {
+
+    printf("Enter a number and opperator ( S, E, +, -, *, /, %% )\n");
+    scanf("%d %c", &n, &op);
+
+    if(op != 'E'){
+      if(op == 'S'){
+        accu = n;
+      }
+      if(op == '+'){
+        res = accu + n;
+        printf("The result of %d + %d is %d\n",accu,n, res);
+        accu = res;
+      }
+      if(op == '-'){
+        res = accu - n;
+        printf("The result of %d - %d is %d\n",accu,n, res);
+        accu = res;
+      }
+      if(op == '*'){
+        res = accu * n;
+        printf("The result of %d * %d is %d\n",accu,n, res);
+        accu = res;
+      }
+      if(op == '/' && n != 0){
+        res = accu / n;
+        printf("The result of %d / %d is %d\n",accu,n, res);
+        accu = res;
+      }
+      if(op == '%' && n != 0){
+        res = accu % n;
+        printf("The result of %d %% %d is %d\n",accu,n, res);
+        accu = res;
+      }
+      op = 0, n = 0;
+    }
+  } while (op != 'E');
+
+  return EXIT_SUCCESS;
 }
