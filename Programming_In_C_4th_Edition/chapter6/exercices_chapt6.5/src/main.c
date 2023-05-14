9a78ecf60dc7ae7feef5bc7d9f8d69bea795b9d9
plgol <plgol.perso@gmail.com>
2023-05-14 10:17:34 +0100

    exo5

--- Programming_In_C_4th_Edition/chapter6/exercices_chapt6.5/src/main.c
+++ Programming_In_C_4th_Edition/chapter6/exercices_chapt6.5/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,18 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+int main(void) {
+  int numbers[10] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int i, j;
 
+  for (j = 0; j < 10; ++j)
+    for (i = 0; i < j; ++i)
+      numbers[j] += numbers[i];
 
-return EXIT_SUCCESS;
+  for (j = 0; j < 10; ++j)
+    printf("%i ", numbers[j]);
+
+  printf("\n");
+
+  return 0;
 }
