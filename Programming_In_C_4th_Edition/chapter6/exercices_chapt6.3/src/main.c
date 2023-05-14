f87fdfd05f46ad68182e2dafd6930a4bea4ff121
plgol <plgol.perso@gmail.com>
2023-05-14 09:52:52 +0100

    exo3

--- Programming_In_C_4th_Edition/chapter6/exercices_chapt6.3/src/main.c
+++ Programming_In_C_4th_Edition/chapter6/exercices_chapt6.3/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,31 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+int main(void) {
+  
+  int ratingCounters[11], i, response;
 
+  for (i = 1; i <= 10; ++i)
+    ratingCounters[i] = 0;
 
-return EXIT_SUCCESS;
+  printf("Enter your responses\n");
+
+  for ( ;  ; ) {
+    scanf("%i", &response);
+    
+    if(response == 999) break;
+    
+    if (response < 1 || response > 10)
+      printf("Bad response: %i\n", response);
+    else
+      ++ratingCounters[response];
+  }
+
+  printf("\n\nRating   Number of Responses\n");
+  printf("------ -------------------\n");
+
+  for (i = 1; i <= 10; ++i)
+    printf("%4i%14i\n", i, ratingCounters[i]);
+
+  return 0;
 }
