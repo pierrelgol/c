eed37c1ddb1eb568f17ed6ddcdb18ba92b577bc1
plgol <plgol.perso@gmail.com>
2023-05-14 12:06:08 +0100

    exo2

--- Programming_In_C_4th_Edition/chapter7/exercices_chapt7.2/src/main.c
+++ Programming_In_C_4th_Edition/chapter7/exercices_chapt7.2/src/main.c
@@ -19,9 +19,28 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
+/* . Modify Program 7.4 so the value of triangularNumber is returned by the
+     function. Then go back to Program 4.5 and change that program so that it calls
+     the new version of the calculateTriangularNumber() function. */
+
+int calculateTriangularNumber(int n) {
+  int i, triangularNumber = 0;
+  for (i = 1; i <= n; ++i)
+    triangularNumber += i;
+	return (triangularNumber);
+}
+
+int main(void)
 {
-
-
-return EXIT_SUCCESS;
+  int number, triangularNumber, counter;
+  
+  for (counter = 1; counter <= 5; ++counter) {
+		 
+    printf("What triangular number do you want? ");
+    scanf("%i", &number);
+    triangularNumber = 0;
+		triangularNumber = calculateTriangularNumber(number);
+    printf("Triangular number %i is %i\n\n", number, triangularNumber);
+  }
+  return 0;
 }
