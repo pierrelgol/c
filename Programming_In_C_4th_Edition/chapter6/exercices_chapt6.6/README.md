7863f12efa43f63d0dafbf31f94c2921c2dcc4a9
plgol <plgol.perso@gmail.com>
2023-05-14 10:29:22 +0100

    exo6

--- Programming_In_C_4th_Edition/chapter6/exercices_chapt6.6/README.md
+++ Programming_In_C_4th_Edition/chapter6/exercices_chapt6.6/README.md
@@ -1,1 +1,1 @@
-# new_project_template
+# 6. You don’t need to use an array to generate Fibonacci numbers. You can simply use three variables: two to store the previous two Fibonacci numbers and one to store the current one. Rewrite Program 6.3 so that arrays are not used. Because you’re no longer using an array, you need to display each Fibonacci number as you generate it.
--- Programming_In_C_4th_Edition/chapter6/exercices_chapt6.6/src/main.c
+++ Programming_In_C_4th_Edition/chapter6/exercices_chapt6.6/src/main.c
@@ -14,13 +14,26 @@
 
 //--------------------[     DEPENDENCIES DECLARATION     ]--------------------//
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
+#define NTH_FIB 20
+
 int main()
 {
+	uint64_t fib_Nm2 = 0, fib_Nm1 = 1 , fib_Nm0 = 0;
+
+	for( size_t i = NTH_FIB; i != 0; --i ){
+		fib_Nm0 = fib_Nm2 + fib_Nm1;
+		fib_Nm2 = fib_Nm1;
+		fib_Nm1 = fib_Nm0;
+		printf("%lu, ",fib_Nm0);
+		fib_Nm0 = 0;
+	}
 
 
 return EXIT_SUCCESS;
