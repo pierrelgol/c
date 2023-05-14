638bfcdbec46f0d6ed77c426e54f3ba315344695
Pierre <pierrelgol@gmail.com>
2023-05-14 21:36:36 +0100

    exo5

--- Programming_In_C_4th_Edition/chapter9/exercices_chapt9.4/src/main.c
+++ Programming_In_C_4th_Edition/chapter9/exercices_chapt9.4/src/main.c
@@ -14,44 +14,27 @@
 
 //--------------------[     DEPENDENCIES DECLARATION     ]--------------------//
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-// Function to determine if a character is alphabetic
-bool alphabetic(const char c) {
-  if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-    return true;
-  else
-    return false;
+
+void substring(const char *src, int from, int length, char *result){
+  size_t i = from, j = 0, bound = (from + length);
+  for( ; i < bound ; i++ ){
+    result[j++] = src[i];
+  }
+  result[j] = '\0';
 }
 
-/* Function to count the number of words in a string */
-int countWords(const char string[]) {
-  
-  int i, wordCount = 0;
-  bool lookingForWord = true, alphabetic(const char c);
-  
-  for (i = 0; string[i] != '\0'; ++i)
-    if (alphabetic(string[i])) {
-      if (lookingForWord) {
-        ++wordCount;
-        lookingForWord = false;
-      }
-    } else
-      lookingForWord = true;
-  return wordCount;
-}
+int main(){
 
-int main(void) {
-  const char text1[] = "Well, here goes.";
-  const char text2[] = "And here we go... again.";
-  
-  int countWords(const char string[]);
-  printf("%s - words = %i\n", text1, countWords(text1));
-  printf("%s - words = %i\n", text2, countWords(text2));
-  
-  return 0;
+  char *str = (char*) malloc(sizeof(char) * 4);
+  substring ("character", 4, 3, str);
+  printf("The substring for character is %s \n",str);
+  free(str);
+  return EXIT_SUCCESS;
 }
