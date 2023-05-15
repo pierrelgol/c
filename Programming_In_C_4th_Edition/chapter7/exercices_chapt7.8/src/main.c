b773f5d93a248b0607b1945c30126fbb51ffca08
plgol <plgol.perso@gmail.com>
2023-05-15 09:27:11 +0100

    exo8

--- Programming_In_C_4th_Edition/chapter7/exercices_chapt7.8/src/main.c
+++ Programming_In_C_4th_Edition/chapter7/exercices_chapt7.8/src/main.c
@@ -19,16 +19,19 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
+#include <stdio.h>
+#include <math.h>
+
 // Function to calculate the absolute value of a number
 double absoluteValue(double x) {
   if (x < 0)
     x = -x;
-  return (x);
+  return x;
 }
 
 // Function to compute the square root of a number
-float sqrRoot(const double x) {
-  const double epsilon = .00001;
+double sqrRoot(const double x) {
+  const double epsilon = 0.00001;
   double guess = 1.0;
   while (absoluteValue(guess * guess - x) >= epsilon)
     guess = (x / guess + guess) / 2.0;
@@ -39,18 +42,25 @@ int main() {
 
   double a, b, c, discriminant, x1, x2;
 
-  printf("Enter the value for a, b, c :\n");
-  scanf(" %lf %lf %lf", &a, &b, &c);
+  printf("Enter the values for a, b, c:\n");
+  scanf("%lf %lf %lf", &a, &b, &c);
 
-  discriminant = sqrRoot(((b * b) - (4 * a * c)));
+  if (a == 0) {
+    printf("The equation is not quadratic.\n");
+  } else {
+    discriminant = (b * b) - (4 * a * c);
 
-  if ( discriminant < 0 ){    
-    printf("The discriminant are imaginary\n");
-  }else{
-    x1 = (-b + sqrRoot((b * b) - (4 * a * c))) / ( 2 * a);
-    x2 = (-b - sqrRoot((b * b) - (4 * a * c))) / ( 2 * a);
-    printf("The roots of the equation are : x1 = %lf | x2 = %lf\n",x1,x2);
+    if (discriminant < 0) {    
+      printf("The roots are imaginary.\n");
+    } else if (discriminant == 0) {
+      x1 = -b / (2 * a);
+      printf("The equation has a single real root: x = %lf\n", x1);
+    } else {
+      x1 = (-b + sqrRoot(discriminant)) / (2 * a);
+      x2 = (-b - sqrRoot(discriminant)) / (2 * a);
+      printf("The roots of the equation are: x1 = %lf, x2 = %lf\n", x1, x2);
+    }
   }
 
-  return EXIT_SUCCESS;
+  return 0;
 }
--- Programming_In_C_4th_Edition/chapter7/exercices_chapt7.9/src/main.c
+++ Programming_In_C_4th_Edition/chapter7/exercices_chapt7.9/src/main.c
@@ -21,7 +21,25 @@
 
 int main()
 {
+	int u, v ,lcm;
 
+	printf("Enter the value for u and v : \n");
+	scanf(" %d %d",&u ,&v);
+
+	lcm = u*v;
+	int min = (u < v ? u : v);
+
+	while(lcm != 0){
+
+		if ( lcm % u == 0 && lcm % v == 0){
+			break;
+		}
+		lcm -= min;
+	}
+
+	if(lcm != 0){
+		printf("The lcm for %d and %d is %d\n",u,v,lcm);
+	}	
 
 return EXIT_SUCCESS;
 }
