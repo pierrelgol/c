c4bc679a10edcbb052d4d8fbc3a6a8daaa86e9b0
plgol <plgol.perso@gmail.com>
2023-05-15 10:21:00 +0100

    exo11

--- Programming_In_C_4th_Edition/chapter7/exercices_chapt7.11/README.md
+++ Programming_In_C_4th_Edition/chapter7/exercices_chapt7.11/README.md
@@ -1,1 +1,1 @@
-# new_project_template
+# 11. Write a function called arraySum() that takes two arguments: an integer array and the number of elements in the array. Have the function return as its result the sum of the elements in the array.
--- Programming_In_C_4th_Edition/chapter7/exercices_chapt7.11/src/main.c
+++ Programming_In_C_4th_Edition/chapter7/exercices_chapt7.11/src/main.c
@@ -14,14 +14,31 @@
 
 //--------------------[     DEPENDENCIES DECLARATION     ]--------------------//
 
+#include <stddef.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
+int64_t arraySum( int *arr, size_t size){
+ int64_t sum = 0;
+ int* end = arr+size;
+ for( ; arr < end; arr++ ){
+  sum += *arr;
+ }
+ return sum;
+}
+
 int main()
 {
+ int *arr = (int*)malloc(sizeof(int) * 10);
 
+ for(int i = 0; i < 10; i++){
+  arr[i] = i;
+ }
 
+ printf("the sum of arr is %li\n",arraySum(arr,10));
+ free(arr);
 return EXIT_SUCCESS;
 }
