05040344f809b68f82e109b697c2281dcb91702a
plgol <plgol.perso@gmail.com>
2023-05-24 14:25:40 +0100

    exo

--- Programming_In_C_4th_Edition/chapter9/exercices_chapt9.13/README.md
+++ Programming_In_C_4th_Edition/chapter9/exercices_chapt9.13/README.md
@@ -1,1 +1,6 @@
-# new_project_template
+# “13. If c is a lowercase character, the expression
+```C
+c – 'a' + 'A'
+```
+produces the uppercase equivalent of c, assuming an ASCII character set.
+Write a function called uppercase() that converts all lowercase characters in a string into their uppercase equivalents.”
--- Programming_In_C_4th_Edition/chapter9/exercices_chapt9.13/src/main.c
+++ Programming_In_C_4th_Edition/chapter9/exercices_chapt9.13/src/main.c
@@ -19,9 +19,16 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
+char toUperCase(char ch){
+ if ( ch >= 'A' && ch <= 'Z') return ch;
+ if ( ch >= 'a' && ch <= 'z') return ( ch - 'a' + 'A');
+ return '\a';
+}
+
+
 int main()
 {
-
+ printf("c to uppercase = %c\n",toUperCase('c'));
 
 return EXIT_SUCCESS;
 }
