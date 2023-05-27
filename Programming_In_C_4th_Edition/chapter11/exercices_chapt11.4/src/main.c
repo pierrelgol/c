57c835c22af08a4478d5cd51f3c029b4b04ba371
PierreLgol <119697612+pierrelgol@users.noreply.github.com>
2023-05-27 15:47:19 +0100

    exo4

--- Programming_In_C_4th_Edition/chapter11/exercices_chapt11.4/src/main.c
+++ Programming_In_C_4th_Edition/chapter11/exercices_chapt11.4/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -19,9 +19,38 @@
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+int main(void) {
+  unsigned int w1 = 0xabcdef00u, w2 = 0xffff1122u;
+  unsigned int rotate(unsigned int value, int n);
+  printf("%x\n", rotate(w1, 8));
+  printf("%x\n", rotate(w1, -16));
+  printf("%x\n", rotate(w2, 4));
+  printf("%x\n", rotate(w2, -2));
+  printf("%x\n", rotate(w1, 0));
+  printf("%x\n", rotate(w1, 44));
+  return 0;
+}
 
+unsigned int insSize(){
+  unsigned int val = ~0, bits = 0;
+  while ( val != 0 ){
+    val >>= 1;
+    ++bits; 
+  }
+  return bits;
+}
 
-return EXIT_SUCCESS;
+unsigned int rotate(unsigned int value, int n) {
+  unsigned int result, bits , intSize = intSize;
+  if (n == 0)
+    result = value;
+  else if (n > 0) { // left rotate
+    bits = value >> (intSize - n);
+    result = value << n | bits;
+  } else { // right rotate
+    n = -n;
+    bits = value << (intSize - n);
+    result = value >> n | bits;
+  }
+  return result;
 }
