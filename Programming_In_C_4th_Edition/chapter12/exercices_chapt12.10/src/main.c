e93d15ad00a1e17965d469dfa99807c640058e47
PierreLgol <119697612+pierrelgol@users.noreply.github.com>
2023-05-30 18:56:37 +0100

    exo10

--- Programming_In_C_4th_Edition/chapter12/exercices_chapt12.10/src/main.c
+++ Programming_In_C_4th_Edition/chapter12/exercices_chapt12.10/src/main.c
@@ -14,14 +14,20 @@
 
 //--------------------[     DEPENDENCIES DECLARATION     ]--------------------//
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
+#define printint(n) printf("%i\n", n)
+
 int main()
 {
-
+	int i = 1, bound = 100;
+	for( ; i <= bound; ++i ){
+		printint(i);
+	}
 
 return EXIT_SUCCESS;
 }
