b70a3347fc997886342655105a0d953b5b19d62a
PierreLgol <119697612+pierrelgol@users.noreply.github.com>
2023-05-31 10:28:31 +0100

    exo2

--- Programming_In_C_4th_Edition/chapter13/exercices_chapt13.2/src/main.c
+++ Programming_In_C_4th_Edition/chapter13/exercices_chapt13.2/src/main.c
@@ -1,14 +1,14 @@
 /*
  *
- *   Title of project : 
- *   Date of creation : 
+ *   Title of project :
+ *   Date of creation :
  *   Author(s) : PierreLgol
  *   github : https://github.com/pierrelgol
- * 
+ *
  *   More on the project :
- *   
- *   #TODO(s) :  
- * 
+ *
+ *   #TODO(s) :
+ *
  *
  */
 
@@ -16,12 +16,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //--------------------[     MAIN METHOD     ]--------------------------------//
 
-int main()
-{
+typedef enum {
+  JAN = 1,
+  FEB,
+  MAR,
+  MAY,
+  JUN,
+  JUL,
+  AUG,
+  SEP,
+  OCT,
+  NOV,
+  DEC,
+} month;
 
+char *month_name(month m);
 
-return EXIT_SUCCESS;
+int main() { 
+
+  month m;
+  char *p;
+
+  puts("Enter a month index to display the string \n");
+  scanf(" %d",&m);
+  p = month_name(m);
+  printf("the index %d correspond to : %s\n",m,p);
+  free(p);
+  return EXIT_SUCCESS;
+}
+
+char *month_name(month m) {
+  if(m >= 1 && m <= 12){
+    const char *months[] = {"Not a month","January",   "february", "March",    "April",
+                            "May",       "June",     "July",     "August",
+                            "September", "October",  "November", "December"};
+    char *p = malloc(sizeof(months[m]));
+    if (p) {
+      strcpy(p, months[m]);
+      return p;
+    }
+  }
+  return NULL;
 }
